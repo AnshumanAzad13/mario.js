9928d7238e647a9873033a94befc61341a549572
AnshumanAzad13 <109653575+AnshumanAzad13@users.noreply.github.com>
2023-02-04 11:12:47 +0400

    Add files via upload

---
+++ mario-in-js-main/js/script.js
@@ -0,0 +1,4 @@
+const body = document.querySelector('body');
+const mario = document.querySelector('#mario');
+const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.cloud');
